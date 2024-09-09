bf563af57ee06ea4fc0eefdd6061ef6aea5a658c
fndsz <45010248+fndsz@users.noreply.github.com>
2024-09-10 01:43:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git mmdvm https://github.com/fndsz/mmdvm-openwrt' feeds.conf.default
 #sed -i '$a src-git mmdvm https://github.com/lazywalker/mmdvm-openwrt' feeds.conf.default
 sed -i '$a src-git devtools https://github.com/lazywalker/devtools-feeds' feeds.conf.default
